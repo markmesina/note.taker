24c1950c4f470a4d463e5a6f75b9caada61fbb3c
ceress <markmesina69@gmail.com>
2020-03-19 19:06:40 -0700

    initialize html routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
-console.log(__dirname)
+const router = require('express').Router();
+
+const apiRoutes = require('./apiRoutes');
+const htmlRoutes = require('./htmlRoutes');
+
+
+//Route here matches "/"
+router.use('/', htmlRoutes);
+//prepends '/api' to routes declared
+// router.use('/api', apiRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// If we are in production (Heroku), process.env.PORT is true, 
-// If we are in development it is false, default to 3000
-const PORT = process.env.PORT || 3000;
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// If we are in production (Heroku), process.env.PORT is true, 
+// If we are in development it is false, default to 3000
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 
 
-app.use(express.static('public'));
+const routes = require('./routes');
+
+app.use(routes);
+
+
+
+// app.use(express.static('public'));
 
 app.listen(PORT);
