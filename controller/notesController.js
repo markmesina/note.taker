c14595a23975e66a4ba318f26dd0baf1a40b1885
ceress <markmesina69@gmail.com>
2020-03-24 13:52:12 -0700

    create AssignID function

--- controller/notesController.js
+++ controller/notesController.js
@@ -6,8 +6,25 @@ module.exports = {
     },
     createNotes: (req,res) => {
          const newNotes = req.body;
+         const id = 0;
+        //  newNotes.id = id +1;
             db.push(newNotes);
-         res.json(newNotes);
-
+            function assignID() {
+                for (i = 0; i < db.length; i ++) {
+                    db[i].id = i;
+                }
+            };
+            assignID();
+            res.json(db);
+        console.log(db);
     },
+    // // deleteNote: (req,res) => {
+    // //     console.log(req.params.id);
+    // //     const noteID = req.params.id;
+    // //     let newID = 0;
+    // //     console.log(`Deleting note with ID ${noteID}`);
+    // //     newNotes = newNotes.filter(currNoted => {
+    // //         return currNoted.id != noteID;
+    // //     })
+    // }
 }
--- routes/apiRoutes/notesApi/index.js
+++ routes/apiRoutes/notesApi/index.js
@@ -2,8 +2,13 @@ const  router = require('express').Router();
 
 const notesController = require('./../../../controller/notesController')
 
+//prepends '/api'
 router.route('/')
     .get(notesController.getNotes)
-    .post(notesController.createNotes)
+    .post(notesController.createNotes);
+
+    //prepends '/api/:'
+// router.route('/:id')
+//     .delete(notesController.deleteNote)
 
 module.exports = router;
